988faa7dae6664afa3d222a6daa51b7dcaf50074
Mikhail Stasyuk <mihail.stasyuk2014@yandex.ru>
2021-07-27 14:25:31 +0500

    Refactored build.bat

--- build.bat
+++ build.bat
@@ -1,18 +1,30 @@
 @echo off
 
-REM set opts=-MT -FC -GR- -Gm- -EHsc- -nologo -Zi -W4
+REM +------------------+
+REM |MSVC Windows build|
+REM +------------------+
 
-REM set link_opts=-opt:ref user32.lib -subsystem:console
 REM set code=%cd%
-REM set opts=-MT -FC -GR- -EHsc- -nologo -Zi -W4 -WX -I"%code%\include" -D_CRT_SECURE_NO_WARNINGS
+REM
+REM set link_opts=-opt:ref user32.lib -subsystem:console
+REM REM set opts=-MT -FC -GR- -EHsc- -nologo -O2 -W4 -WX -D_CRT_SECURE_NO_WARNINGS
+REM set opts=-MT -FC -GR- -EHsc- -nologo -Zi -W4 -WX -D_CRT_SECURE_NO_WARNINGS
+REM
+REM pushd build
+REM cl %opts% %code%\main.c -Fetetris -link %link_opts%
+REM popd
 
-set link_opts=-luser32 -Xlinker /subsystem:console -Xlinker /implib:tetris.lib -Xlinker /pdb:tetris.pdb
+
+REM +-------------------+
+REM |Clang Windows build|
+REM +-------------------+
+
 set code=%cd%
 
-REM set opts=-Wall -Wextra -Werror -pedantic -O3 -I"%code%\include" -D_DLL -D_MT -nostdlib -Xclang --dependent-lib=msvcrt -DNDEBUG -MD -MT -D_CRT_SECURE_NO_WARNINGS
-set opts=-Wall -Wextra -Werror -pedantic -g -O0 -I"%code%\include" -MD -MT -D_DLL -D_MT -D_CRT_SECURE_NO_WARNINGS -nostdlib -Xclang --dependent-lib=msvcrtd  
+set link_opts=-luser32 -Xlinker /subsystem:console -Xlinker /implib:tetris.lib -Xlinker /pdb:tetris.pdb
+REM set opts=-Wall -Wextra -Werror -pedantic -O3 -MD -MT -D_CRT_SECURE_NO_WARNINGS -D_DLL -D_MT -DNDEBUG  -nostdlib -Xclang --dependent-lib=msvcrt 
+set opts=-Wall -Wextra -Werror -pedantic -g -O0 -MD -MT -D_CRT_SECURE_NO_WARNINGS -D_DLL -D_MT -nostdlib -Xclang --dependent-lib=msvcrtd
 
 pushd build
-REM clang-cl %opts% %code%\main.c -Fetetris -link %link_opts%
 clang %opts% %code%\main.c -o tetris.exe %link_opts%
 popd
