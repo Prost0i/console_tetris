facd2553ec2f6748e585088081932b8b997367d7
Mikhail Stasyuk <mihail.stasyuk2014@yandex.ru>
2021-08-03 18:04:22 +0500

    Separate release and debug build batch scripts

--- build_rel.bat
+++ build_rel.bat
@@ -1,10 +1,10 @@
 @echo off
 
 set code=%cd%
-set build_dir=build
+set build_dir=build_rel
 
 set link_opts=-luser32 -Xlinker /subsystem:console -Xlinker /implib:tetris.lib -Xlinker /pdb:tetris.pdb
-set opts=-Wall -Wextra -Werror -pedantic -g -O0 -MD -MT -D_CRT_SECURE_NO_WARNINGS -D_DLL -D_MT -nostdlib -Xclang --dependent-lib=msvcrtd
+set opts=-Wall -Wextra -Werror -pedantic -O3 -MD -MT -D_CRT_SECURE_NO_WARNINGS -D_DLL -D_MT -DNDEBUG  -nostdlib -Xclang --dependent-lib=msvcrt 
 
 if not exist %build_dir% mkdir %build_dir%
 pushd %build_dir%
